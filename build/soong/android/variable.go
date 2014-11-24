5051281d03f068e318adccb0cce4c1f1bdf83124
Michael Bestas <mikeioannina@gmail.com>
2014-11-24 22:12:22 +0200

    soong: add non pie support
    Change-Id: Ie9e754d5610249b6791c6efff4115d8918d6a682

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -22,6 +22,9 @@ type Product_variables struct {
 	Needs_prelink_support struct {
 		Cppflags []string
 	}
+	Needs_non_pie_support struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -33,4 +36,5 @@ type ProductVariables struct {
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 	Has_legacy_mmap  *bool `json:",omitempty"`
 	Needs_prelink_support  *bool `json:",omitempty"`
+	Needs_non_pie_support  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -10,6 +10,7 @@ aosp_soong:
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
 	echo '    "Has_legacy_mmap": $(if $(filter true,$(BOARD_USES_LEGACY_MMAP)),true,false),'; \
 	echo '    "Needs_prelink_support": $(if $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)),true,false),'; \
+	echo '    "Needs_non_pie_support": $(if $(filter true,$(TARGET_NEEDS_NON_PIE_SUPPORT)),true,false),'; \
 	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
