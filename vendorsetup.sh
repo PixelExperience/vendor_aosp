c907ed1aa70ce9106e072fc25691fc7f6e1667cc
Joe <joe2k01dev@gmail.com>
2017-07-20 19:27:01 +0200

    vendorsetup: Enable also other build variants

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 for device in $(curl -s https://raw.githubusercontent.com/PixelExperience/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
 do
-add_lunch_combo aosp_$device-userdebug
+for var in eng user userdebug; do
+add_lunch_combo aosp_$device-$var
+done
 done
