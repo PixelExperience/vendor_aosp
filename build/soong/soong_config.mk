001bd71b5e9ab90f09800ff49b3357d522ccad6c
Nick Reuter <nreuter85@gmail.com>
2017-08-22 18:11:35 -0500

    soong: add Has_legacy_mmap product variable
    Change-Id: I743eeac6ec40e356fdeb2e8c4f8957c09e84b434

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -8,6 +8,7 @@ aosp_soong:
 	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
+	echo '    "Has_legacy_mmap": $(if $(filter true,$(BOARD_USES_LEGACY_MMAP)),true,false),'; \
 	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
