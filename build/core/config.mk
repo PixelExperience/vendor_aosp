58c42db9c96b30699eda96f81a28be351f229b06
Joey <joey@lineageos.org>
2018-02-04 20:41:06 +0100

    vendor: add BUILD_RRO_SYSTEM_PACKAGE target
    Allows to ship rro packages in system instead of vendor
    
    Change-Id: I8822ca477e80297dce477de2d87bd3e74d4cbfb0
    Signed-off-by: Joey <joey@lineageos.org>

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,3 +1,5 @@
+BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/aosp/build/core/system_rro.mk
+
 # We modify several neverallows, so let the build proceed
 ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
