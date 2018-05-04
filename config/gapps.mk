9b06912bc3dd390e109129b866d6af6bef7a99e1
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-05-04 22:28:19 +0000

    vendor: Update gapps config
    Change-Id: Ibcb7167a9da8763e5b33d2d80847376c03312457

--- config/gapps.mk
+++ config/gapps.mk
@@ -2,22 +2,20 @@ TARGET_MINIMAL_APPS ?= false
 
 GAPPS_VARIANT := nano
 
-GAPPS_FORCE_MATCHING_DPI := true
-
-ifeq ($(TARGET_HAS_NFC_SUPPORT),true)
-GAPPS_PRODUCT_PACKAGES += TagGoogle
-endif
-
 ifeq ($(IS_PHONE),true)
-GAPPS_PRODUCT_PACKAGES += PrebuiltBugle GoogleDialer
+GAPPS_FORCE_DIALER_OVERRIDES := true
+GAPPS_FORCE_MMS_OVERRIDES := true
 endif
 
-GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle
+GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle PrebuiltDeskClockGoogle CalculatorGoogle TagGoogle
+GAPPS_FORCE_WEBVIEW_OVERRIDES := true
+GAPPS_EXCLUDED_PACKAGES := PrebuiltGmsCoreInstantApps
 
 ifeq ($(TARGET_MINIMAL_APPS),false)
-GAPPS_PRODUCT_PACKAGES += CalendarGooglePrebuilt Chrome Photos PrebuiltDeskClockGoogle CalculatorGooglePrebuilt
+GAPPS_FORCE_BROWSER_OVERRIDES := true
+GAPPS_PRODUCT_PACKAGES += CalendarGooglePrebuilt Photos
 else
-GAPPS_EXCLUDED_PACKAGES := Velvet
+GAPPS_EXCLUDED_PACKAGES += Velvet
 endif
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
