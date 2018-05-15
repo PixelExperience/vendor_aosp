2efbd4499b8aec0b3910ae6108d064c9c1fe1471
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-05-15 01:07:55 +0000

    vendor: Add ota version code
    Change-Id: Ic025e5f70a1390fc683e4c116adde7c30203bfd2

--- config/ota.mk
+++ config/ota.mk
@@ -1,4 +1,11 @@
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
+
+CUSTOM_OTA_VERSION_CODE ?= oreo
+
+CUSTOM_PROPERTIES += \
+    org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
+
 PRODUCT_PACKAGES += \
     Updates
+
 endif
