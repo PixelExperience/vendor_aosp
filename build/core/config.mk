a113fe0492f2ed034c33efcccb1f50cb7740e18e
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    vendor: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,3 +1,8 @@
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Rules for MTK targets
 include $(TOPDIR)vendor/aosp/build/core/mtk_target.mk
 
