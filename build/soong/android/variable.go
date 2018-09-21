2b2fa66e246c23abb419dbe6556335f449b96dda
Adarsh-MR <adarshmr1998@gmail.com>
2018-09-21 14:37:04 +0000

    vendor: soong: Add target_uses_color_metadata soong flag
    Change-Id: Iae2503475b2d5d273754f0b16fee3bb550dc1f06
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,10 +6,14 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_uses_color_metadata struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Java_Source_Overlays *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_uses_color_metadata  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,6 +6,7 @@ _contents := $(_contents)    "Aosp":{$(newline)
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 $(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
 $(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_uses_color_metadata, $(filter true,$(TARGET_USES_COLOR_METADATA)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
