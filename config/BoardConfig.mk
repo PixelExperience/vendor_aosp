503041ff87fab127a90a44dc6b20e3456213bae0
joshuous <joshuous@gmail.com>
2018-10-01 20:56:01 +0000

    vendor: Set DISABLE_EAP_PROXY to break wpa_supplicant dependency on qmi
    * Without this flag, wpa_supplicant fails to build because we do not have
      sources for these proprietary libs: libqmi_cci, libqmiservices, libidl.
    
    * Reference:
      https://source.codeaurora.org/quic/la/platform/external/wpa_supplicant_8/commit/?h=cfd8fe3d57bcf0976dddb0d82ec04fcd2dbc5ebc
    
    Change-Id: Id161eadd9e5327768d1f41fb621d4422aeaa27ec
    Signed-off-by: joshuous <joshuous@gmail.com>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,3 +1,6 @@
 include vendor/aosp/config/BoardConfigKernel.mk
 include vendor/aosp/config/BoardConfigQcom.mk
 include vendor/aosp/config/BoardConfigSoong.mk
+
+# Disable qmi EAP-SIM security
+DISABLE_EAP_PROXY ?= true
