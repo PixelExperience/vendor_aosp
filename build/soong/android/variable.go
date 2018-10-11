e7e203b4b0b149a76315b6dd89db4eebd753d338
Alessandro Astone <ales.astone@gmail.com>
2018-10-12 00:16:27 +0200

    soong_config: Add BOOTLOADER_MESSAGE_OFFSET
    * libbootloader_message now uses Android.bp
    
    Change-Id: I750a04be97dbe01564d45cd605744b15f209e408

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,9 @@ type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
+	Bootloader_message_offset struct {
+		Cflags []string
+	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
@@ -13,6 +16,7 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+	Bootloader_message_offset  *int `json:",omitempty"`
 	Java_Source_Overlays *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,9 +1,11 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
 
 _contents := $(_contents)    "Aosp":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
 $(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
 $(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Target_uses_color_metadata, $(filter true,$(TARGET_USES_COLOR_METADATA)))
