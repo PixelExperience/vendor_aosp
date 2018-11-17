291e456af9f73e79404d9541dd1cf2e81290326b
Shubham Singh <coolsks94@gmail.com>
2018-11-17 13:13:28 +0530

    vendor: Update vendorsetup logic
    * Only include devices having pie version
    
    Change-Id: I9ee7cd226337df450fdac851ebdea736dc8f6190
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- tools/get_official_devices.py
+++ tools/get_official_devices.py
@@ -19,6 +19,9 @@ try:
     data = json.loads(response.read())
     for res in data:
         if res['codename'] != "treble_gsi":
-            print (res['codename'])
+            for version in res['supported_versions']:
+                if version['version_code'] == 'pie' or version['version_code'] == 'pie_go':
+                    print (res['codename'])
+                    break
 except:
     print ("")
