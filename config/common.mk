31303adc4520d7d1fc0849ca0bb31e994aa7b4c4
Han Wang <416810799@qq.com>
2019-01-06 06:53:03 +0100

    config: Add Deskclock to power whitelist
     * Seems sometimes alarm cannot ring because of doze.
     * Also add GoogleDeskClock - @jhenrique09
    
    Change-Id: I3a35d2f02de365ea1d3a99ea75e0db9905d482a3

--- config/common.mk
+++ config/common.mk
@@ -91,6 +91,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# Power whitelist
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
