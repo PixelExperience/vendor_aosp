20e09544dc5cf598eac0b66e1bdb5593c6782790
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-03 12:25:55 +0100

    Never allow neverallows
    Change-Id: Ibf07e9e319d676666e3e7389720c24917e38a9ac

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,10 +1,5 @@
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/aosp/build/core/system_rro.mk
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for MTK targets
 include $(TOPDIR)vendor/aosp/build/core/mtk_target.mk
 
