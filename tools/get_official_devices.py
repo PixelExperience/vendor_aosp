749db97bf2b9fa5ef9f7008ecd016bf0bc71c68b
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-19 14:51:16 -0300

    vendor: Update default branch
    Change-Id: I129c871a03938cff8b571e7b3266a9ef5d2c9a67

--- tools/get_official_devices.py
+++ tools/get_official_devices.py
@@ -19,7 +19,7 @@ try:
     data = json.loads(response.read())
     for res in data:
         for version in res['supported_versions']:
-            if version['version_code'] == 'pie' or version['version_code'] == 'pie_go':
+            if version['version_code'] == 'pie_caf':
                 print (res['codename'])
                 break
 except:
