317c69c4bccc08ad56dba3d327cf23639a34967d
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-19 00:26:15 -0300

    vendor: Import sdclang configuration from PA
    Change-Id: If1b72992019dfd76d1f59e7ca06f6b10c195c958

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,8 @@ for var in eng user userdebug; do
 add_lunch_combo aosp_$device-$var
 done
 done
+
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=vendor/aosp/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=vendor/aosp/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false
