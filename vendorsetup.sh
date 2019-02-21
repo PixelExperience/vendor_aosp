4927056309dde6c81c4432806a60482c606f93eb
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-20 21:24:51 -0300

    Remove eng builds from lunch
    Change-Id: Iab4c6912eb2d86bb8506517993f3e7c503880b70

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 for device in $(python vendor/aosp/tools/get_official_devices.py)
 do
-for var in eng user userdebug; do
+for var in user userdebug; do
 add_lunch_combo aosp_$device-$var
 done
 done
