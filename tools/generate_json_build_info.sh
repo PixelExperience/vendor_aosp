b30f11953719883845572ad7c79598cf55ae88d7
Anderson Mendes <anderson2000ano@gmail.com>
2019-03-05 09:37:44 -0300

    generate_json_build_info: Update spacing
    match with the formatter

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -6,6 +6,6 @@ then
   if [ -f $file_path ]; then
     file_size=$(stat --printf="%s" "$file_path")
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    echo "{\n   \"file_name\":\"$file_name\",\n   \"file_size\":$file_size,\n   \"md5\":\"$md5\"\n}" > $file_path.json
+    echo "{\n   \"file_name\": \"$file_name\",\n   \"file_size\": $file_size,\n   \"md5\": \"$md5\"\n}" > $file_path.json
   fi
 fi
