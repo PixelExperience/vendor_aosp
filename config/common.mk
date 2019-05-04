2a818e36ef5ae4a9f71ea5c331e3f037d63af256
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-04 00:25:31 -0300

    vendor: Whitelist LiveDisplay permissions
    Change-Id: Iefa5365b6ec013f164a338038b0d13c45b369a13

--- config/common.mk
+++ config/common.mk
@@ -62,7 +62,8 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
     vendor/aosp/config/permissions/privapp-permissions-aosp.xml:system/etc/permissions/privapp-permissions-aosp.xml \
-    vendor/aosp/config/permissions/org.lineageos.snap.xml:system/etc/permissions/org.lineageos.snap.xml
+    vendor/aosp/config/permissions/org.lineageos.snap.xml:system/etc/permissions/org.lineageos.snap.xml \
+    vendor/aosp/config/permissions/privapp-permissions-custom.xml:system/etc/permissions/privapp-permissions-custom.xml
 
 # init.d support
 PRODUCT_COPY_FILES += \
