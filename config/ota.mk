b42377885676922dec4b7164bbcf3021b3deda55
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-23 14:32:48 -0300

    Remove "GO" custom version
    Maintainers should choice if normal edition should have Android Go props
    
    Change-Id: I77ab4b62d41e82f5f608bfde7f771ff0a691975c

--- config/ota.mk
+++ config/ota.mk
@@ -1,11 +1,7 @@
 ifneq ($(IS_GENERIC_SYSTEM_IMAGE), true)
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 
-ifeq ($(IS_GO_VERSION), true)
-CUSTOM_OTA_VERSION_CODE := pie_go
-else
 CUSTOM_OTA_VERSION_CODE := pie
-endif
 
 CUSTOM_PROPERTIES += \
     org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
