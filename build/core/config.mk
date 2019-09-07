fa04e757c8a56ef739454b1ba7e5a2f9c6f632f5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-07 19:24:55 -0300

    [TEMP] vendor: Allow neverallows for now
    For bringup
    
    Change-Id: I2010d0943a94af8693f6141e16441a675430d206

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,4 +1,9 @@
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/aosp/build/core/system_rro.mk
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Rules for QCOM targets
 include $(TOPDIR)vendor/aosp/build/core/qcom_target.mk
