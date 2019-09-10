e0be31c85d8ebe6c3da2a8fdcb7e40e3d2d4331e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 15:39:05 -0300

    vendor: branding: Add org.pixelexperience.version.display prop
    Change-Id: Ie8834dd401ca76c08023ca5d8387d44f6a57a2d7

--- config/branding.mk
+++ config/branding.mk
@@ -19,6 +19,7 @@ ROM_FINGERPRINT := PixelExperience_Plus/$(CUSTOM_PLATFORM_VERSION)/$(TARGET_PROD
 
 CUSTOM_PROPERTIES := \
     org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
+    org.pixelexperience.version.display=$(CUSTOM_VERSION) \
     org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
     org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
     org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE) \
