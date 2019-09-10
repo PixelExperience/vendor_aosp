a6133102da892055e72e33f13834f07947e47757
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-09 23:49:45 -0300

    vendor: Also add eng variant to lunch menu
    Change-Id: I1db11d61bb68e350418912948553ae58b2976be8

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 lunch_others_targets=()
 for device in $(python vendor/aosp/tools/get_official_devices.py)
 do
-    for var in user userdebug; do
+    for var in user userdebug eng; do
         lunch_others_targets+=("aosp_$device-$var")
     done
 done
