a2e37aba214b8577f9a49686c3ae9f69043e01f8
Bruno Martins <bgcngm@gmail.com>
2019-09-11 21:54:28 +0100

    envsetup: Remove call to deprecated function
    This allows us to get rid of the following warning when running
    `breakfast` command:
    
    add_lunch_combo is obsolete. Use COMMON_LUNCH_CHOICES in your AndroidProducts.mk instead.
    
    Change-Id: I5b05515465d40b9b77f74c69293a1431c5325f5d

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -67,14 +67,6 @@ function breakfast()
 {
     target=$1
     local variant=$2
-    unset LUNCH_MENU_CHOICES
-    add_lunch_combo full-eng
-    for f in `/bin/ls vendor/aosp/vendorsetup.sh 2> /dev/null`
-        do
-            echo "including $f"
-            . $f
-        done
-    unset f
 
     if [ $# -eq 0 ]; then
         # No arguments, so let's have the full menu
