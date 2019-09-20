e807c11195741e7dfaf005b62b4913b3c88bcbfe
Marko Man <darkobas@gmail.com>
2019-09-20 12:55:17 +0200

    vendor: soong: add target_needs_client_info
    Change-Id: Ie69e52ab32f2eb4970ffa26414184ec9f1d5895c

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -40,6 +40,9 @@ type Product_variables struct {
 	Target_needs_netd_direct_connect_rule struct {
 		Cppflags []string
 	}
+	Target_camera_needs_client_info struct {
+		Cflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -97,6 +100,7 @@ type ProductVariables struct {
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
+	Target_camera_needs_client_info  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
