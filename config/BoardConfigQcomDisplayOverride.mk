e3e12b0e5f6beea05ac120d0af130cda0961e825
Dyneteve <sound0020@gmail.com>
2019-09-27 15:11:44 +0000

    Revert "vendor: Add special conditions for devices with prebuilt display hal"
    This reverts commit 8a9cf11622e671e4115f831f1e30353164fad23a.

--- config/BoardConfigQcomDisplayOverride.mk
+++
@@ -1,5 +0,0 @@
-QCOM_HARDWARE_VARIANT := $(FORCE_QCOM_DISPLAY_HAL_VARIANT)
-
-include vendor/aosp/config/BoardConfigQcomUmPlatform.mk
-
-PRODUCT_SOONG_NAMESPACES += hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT)
