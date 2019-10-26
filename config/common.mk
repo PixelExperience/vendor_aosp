02c0b2b3f7a6dfb8c1e086387d47c51beeecdfa9
Han Wang <416810799@qq.com>
2019-10-26 16:54:06 +0200

    config: Remove unnecessary persist.sys.dun.override
    Change-Id: I2f82e283a66992fe9e108d399eb7e9d135b54387

--- config/common.mk
+++ config/common.mk
@@ -27,11 +27,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.config.notification_sound=Popcorn.ogg \
     ro.config.alarm_alert=Bright_morning.ogg
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# Thank you, please drive thru!
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.dun.override=0
-endif
-
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
