e235cf7bb4e682d596a4435b4de9e1b7623a3c30
Dyneteve <dyneteve@pixelexperience.org>
2019-11-02 19:19:07 +0000

    config: Use tools-custom instead of tools-lineage
    * We don't track it anymore
    
    Change-Id: I7233dbfc71b8e2a9bde8667aec73c7de6286facc

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -126,9 +126,9 @@ ifneq ($(TARGET_KERNEL_ADDITIONAL_FLAGS),)
 endif
 
 TOOLS_PATH_OVERRIDE := \
-    PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_OS)-x86/bin:$$PATH \
-    LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-lineage/common/perl-base
+    PATH=$(BUILD_TOP)/prebuilts/tools-custom/$(HOST_OS)-x86/bin:$$PATH \
+    LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-custom/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-custom/common/perl-base
 
 # Set DTBO image locations so the build system knows to build them
 ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)
