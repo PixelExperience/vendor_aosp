1e11c6cec70693249ae7ec28144c5c877caca48c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-04 15:48:17 +0000

    vendor: Add signing parameters
    Change-Id: I668074536cdac25a625d537a6da8424126227098

--- build/core/config.mk
+++ build/core/config.mk
@@ -2,3 +2,9 @@ BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/aosp/build/core/system_rro.mk
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/aosp/build/core/qcom_target.mk
+
+# Signing
+ifneq (${CUSTOM_KEYS_PATH},)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := ${CUSTOM_KEYS_PATH}/releasekey
+    PRODUCT_EXTRA_RECOVERY_KEYS := $(PRODUCT_DEFAULT_DEV_CERTIFICATE)
+endif
