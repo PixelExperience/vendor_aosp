7277e63b7c2bec512eff2a6ba2c26ff90a54d4e3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-06 00:47:14 +0000

    vendor: changelog: Update date format
    Change-Id: I29bf0e9ada0d91ef458d6a02c380381de95a75a9

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -13,9 +13,9 @@ echo "Generating changelog..."
 
 for i in $(seq 14);
 do
-export After_Date=`date --date="$i days ago" +%m-%d-%Y`
+export After_Date=`date --date="$i days ago" +%Y/%m/%d`
 k=$(expr $i - 1)
-	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
+	export Until_Date=`date --date="$k days ago" +%Y/%m/%d`
 
 	# Line with after --- until was too long for a small ListView
 	echo '=======================' >> $Changelog;
