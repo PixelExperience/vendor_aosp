42553b012f8ae378ea1cb2b7692fc4d902fc8642
Dyneteve <dyneteve@pixelexperience.org>
2019-12-07 17:03:07 +0200

    vendor: Drop backuptools
    Change-Id: Id1712f5c409041e00d32c67b7ff03f5fe9a215c0

--- config/common.mk
+++ config/common.mk
@@ -44,20 +44,6 @@ endif
 #PRODUCT_PACKAGES += \
 #    AmbientPlayHistoryProvider
 
-# Backup Tool
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/aosp/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
-    vendor/aosp/prebuilt/common/bin/blacklist:system/addon.d/blacklist
-
-ifneq ($(AB_OTA_PARTITIONS),)
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
-    vendor/aosp/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
-    vendor/aosp/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
-endif
-
 # Some permissions
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
