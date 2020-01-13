57b55d889417895096d14815c974cc9f388daa75
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-01-13 16:03:00 +0000

    vendor: Update recovery on official targets
    Change-Id: If5ffe98066d146c51904c0313cc6b9ab249f12a6

--- config/ota.mk
+++ config/ota.mk
@@ -3,7 +3,8 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 CUSTOM_OTA_VERSION_CODE := ten
 
 CUSTOM_PROPERTIES += \
-    org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
+    org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
+    persist.sys.recovery_update=true
 
 PRODUCT_PACKAGES += \
     Updates
