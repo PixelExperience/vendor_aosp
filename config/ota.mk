8d61fbdce6e038be822e932ef507510bc030f04c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-02 18:55:11 -0300

    [DNM] vendor: Enable ota for unofficial
    Change-Id: I972871ee72b1cf8584644b25b3f4293362fdea3c

--- config/ota.mk
+++ config/ota.mk
@@ -1,5 +1,3 @@
-ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
-
 CUSTOM_OTA_VERSION_CODE := ten
 
 CUSTOM_PROPERTIES += \
@@ -10,5 +8,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/org.pixelexperience.ota.xml:system/etc/permissions/org.pixelexperience.ota.xml
-
-endif
