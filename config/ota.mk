0cfa29df55c3d49ea901e051cedda4454bc080c5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-05 08:12:36 -0300

    vendor: Changes for plus version
    Change-Id: Ie12f35fbd73f186dfa7b0da201b57c56df6ffc66

--- config/ota.mk
+++ config/ota.mk
@@ -1,6 +1,6 @@
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 
-CUSTOM_OTA_VERSION_CODE := ten
+CUSTOM_OTA_VERSION_CODE := ten_plus
 
 CUSTOM_PROPERTIES += \
     org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
