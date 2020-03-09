9ecb12082c0613f60822fbd578d8b3f746b77f23
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-08 23:23:14 -0300

    Revert "Revert "vendor: Add hide notch support [2/3]""
    This reverts commit 76759a11460881f4107dc03fda471a151e03b1b3.
    
    Change-Id: I1474bf4deb83bcbf714905e820485e0559a1b776

--- config/common.mk
+++ config/common.mk
@@ -114,6 +114,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aosp/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
 
+# Cutout control overlay
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # PixelSetupWizard overlay
 PRODUCT_PACKAGES += \
     PixelSetupWizardOverlay \
