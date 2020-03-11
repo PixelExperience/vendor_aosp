MoKee/android_vendor_mk

extract_utils: Fix bad substitution for zsh
Change-Id: I5f4fabc62b1ac3da159b36c3ae6080e145948530

build/tools/extract_utils.sh
@@ -1177,7 +1177,7 @@ function oat2dex() {
     local SRC="$3"
     local TARGET=
     local OAT=
-    local HOST="$(uname)"
+    local HOST="$(uname | tr '[:upper:]' '[:lower:]')"
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
         export BAKSMALIJAR="$MOKEE_ROOT"/prebuilts/tools-mokee/common/smali/baksmali.jar
@@ -1185,11 +1185,11 @@ function oat2dex() {
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$MOKEE_ROOT"/prebuilts/tools-mokee/"${HOST,,}"-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$MOKEE_ROOT"/prebuilts/tools-mokee/${HOST}-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$MOKEE_ROOT"/prebuilts/tools-mokee/"${HOST,,}"-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$MOKEE_ROOT"/prebuilts/tools-mokee/${HOST}-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder

PixelExperience/vendor_aosp

build/tools/extract_utils.sh
@@ -1199,7 +1199,7 @@ function oat2dex() {
     local SRC="$3"
     local TARGET=
     local OAT=
-    local HOST="$(uname)"
+    local HOST="$(uname | tr '[:upper:]' '[:lower:]')"
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
         export BAKSMALIJAR="$AOSP_ROOT"/prebuilts/tools-custom/common/smali/baksmali.jar
@@ -1207,11 +1207,11 @@ function oat2dex() {
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$AOSP_ROOT"/prebuilts/tools-custom/"${HOST,,}"-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$AOSP_ROOT"/prebuilts/tools-custom/${HOST}-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$AOSP_ROOT"/prebuilts/tools-custom/"${HOST,,}"-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$AOSP_ROOT"/prebuilts/tools-custom/${HOST}-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder