cb477a42fe8e0fd7dd4b0f13c4a752d26b42e62d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-22 11:33:40 -0300

    vendor: Adapt to new ota API
    Change-Id: I9f0d03ca78bcfb2603441892747e0d0bf024a0f4

--- config/ota.mk
+++ config/ota.mk
@@ -1,10 +1,5 @@
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 
-CUSTOM_OTA_VERSION_CODE := ten
-
-CUSTOM_PROPERTIES += \
-    org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
-
 PRODUCT_PACKAGES += \
     Updates
 
