aa622d159259c80398e7af2553a58276d66afc9c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-25 03:18:58 -0300

    vendor: Build Recorder
    Change-Id: Ifb0dfeff155f3f24cd55d1f42edea00460c1c113

--- config/common.mk
+++ config/common.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
     misc_writer_system \
     themed_bootanimation
 
+# Screen recorder
+PRODUCT_PACKAGES += \
+    Recorder
+
 # Branding
 include vendor/aosp/config/branding.mk
 
