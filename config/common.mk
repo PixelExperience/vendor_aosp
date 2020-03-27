d8a185d76e6c7f3d7a4278e12f236090055f0176
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    [2/5] Port face unlock feature
    Build face unlock for arm64 devices
    
    Also add a flag to opt-out
    
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6

--- config/common.mk
+++ config/common.mk
@@ -150,6 +150,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Recorder
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifeq ($(TARGET_GAPPS_ARCH),arm64)
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # Branding
 include vendor/aosp/config/branding.mk
 
