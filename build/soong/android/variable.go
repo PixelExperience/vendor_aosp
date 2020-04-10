dc7ff26874d44b494691b26e023ffd02c5d61e49
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-04-10 14:56:31 -0300

    Revert "vendor: Revert caf sf commits"
    This reverts commit 820028b2a70cc72285d7829149977b74d12fe3b3.
    
    Reason for revert: CAF sf still neeeded to fix screen recording lags
    
    Change-Id: I71cfdfacaf93ac3616f8793d3ccc87a13f1fb8a8

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -59,6 +59,30 @@ type Product_variables struct {
 		Cppflags []string
 		Shared_libs []string
 	}
+	Uses_qcom_um_family struct {
+		Cflags []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_3_18_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_4_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_9_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_14_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
 	Needs_camera_boottime_timestamp struct {
 		Cflags []string
 		Srcs []string
@@ -85,5 +109,10 @@ type ProductVariables struct {
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
+	Uses_qcom_um_family  *bool `json:",omitempty"`
+	Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
+	Uses_qcom_um_4_4_family  *bool `json:",omitempty"`
+	Uses_qcom_um_4_9_family  *bool `json:",omitempty"`
+	Uses_qcom_um_4_14_family  *bool `json:",omitempty"`
 	Needs_camera_boottime_timestamp  *bool `json:",omitempty"`
 }
