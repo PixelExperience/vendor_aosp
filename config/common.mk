e398115e3de2790dc7aa84e8fe13717f8df6924f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-30 12:44:39 -0300

    vendor: Fix google/customization path
    Change-Id: I61e1dd2c01d6ca2c7e5e71283949a26a3c5cc055

--- config/common.mk
+++ config/common.mk
@@ -179,6 +179,6 @@ include vendor/gapps/config.mk
 include vendor/pixelstyle/config.mk
 
 # Customization
-include vendor/google/customization/config.mk
+include vendor/google-customization/config.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
