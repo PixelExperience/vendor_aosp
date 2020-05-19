9c0fac99ca9f353ce2f677bbb7fae45701e7daf7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-05-19 17:17:41 -0300

    Cleanup permissions
    Change-Id: I15d4e7cb670c7771916c38d5616b1ff8c4b8e996
    (cherry picked from commit 8c810b37ad77aa404643733e7f86d63cac9174ae)

--- config/common.mk
+++ config/common.mk
@@ -51,10 +51,7 @@ endif
 
 # Some permissions
 PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
-    vendor/aosp/config/permissions/privapp-permissions-fm.xml:system/etc/permissions/privapp-permissions-fm.xml \
-    vendor/aosp/config/permissions/privapp-permissions-snap.xml:system/etc/permissions/privapp-permissions-snap.xml \
-    vendor/aosp/config/permissions/privapp-permissions-camera2.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-camera2.xml
+    vendor/aosp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
 
 # Copy all custom init rc files
 $(foreach f,$(wildcard vendor/aosp/prebuilt/common/etc/init/*.rc),\
