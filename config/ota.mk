6d9d6e401d4e00057779fcc8ee2deb062edf9237
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-05-20 00:41:27 -0300

    vendor: Nuke ota permission
    Move to package repo
    
    Change-Id: Ic7d36a7857eba3352cb62b07dd94ed8464fd09c4
    (cherry picked from commit babdf0a406b2317985f749a802d6e0429d07421d)

--- config/ota.mk
+++ config/ota.mk
@@ -3,7 +3,4 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 PRODUCT_PACKAGES += \
     Updates
 
-PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/org.pixelexperience.ota.xml:system/etc/permissions/org.pixelexperience.ota.xml
-
 endif
