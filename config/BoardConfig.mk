3445520351d7be54077a9b59285436719868ec37
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-13 13:36:56 -0300

    Revert "vendor: Allow providing camera server and service"
    This reverts commit db239129f60e9659d3f9caa1d8e299f2e1e7d50a.
    
    Change-Id: Ic7852194e9bfd081e1c866a8ee5f38fd55fd1ef3
    (cherry picked from commit ae5a23e56af2954a69e2b7a3a1d90b873ad5d79b)

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -6,12 +6,6 @@ endif
 
 include vendor/aosp/config/BoardConfigSoong.mk
 
-ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
-PRODUCT_SOONG_NAMESPACES += \
-    frameworks/av/camera/cameraserver \
-    frameworks/av/services/camera/libcameraservice
-endif
-
 # Namespace for fwk-detect
 TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
 PRODUCT_SOONG_NAMESPACES += \
