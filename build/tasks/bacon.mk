e39d2a9140625f8a84f9fcd5253478815628cd35
Jan Altensen <info@stricted.net>
2020-09-11 21:41:08 +0200

    bacon: switch to toybox md5sum
     * fixes "error: MD5SUM is obsolete."
    
    Change-Id: I38e167e24e9eb5596a1e8e2f2d915f648bc1283c

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -16,8 +16,10 @@
 
 CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CUSTOM_VERSION).zip
 
+MD5 := prebuilts/build-tools/path/$(HOST_OS)-x86/md5sum
+
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_TARGET_PACKAGE)
-	$(hide) $(MD5SUM) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).md5sum
+	$(hide) $(MD5) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).md5sum
 	@echo "Package Complete: $(CUSTOM_TARGET_PACKAGE)" >&2
