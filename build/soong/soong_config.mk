8c4aeee4e946093f51d7519b11c419bc68de419a
TheMalachite <eliasgheeraert@gmail.com>
2020-10-18 12:02:18 +0200

    soong_config: Add BOARD_BUILD_MTK_HALS
    * This flag is only used for include OSS MTK HALS
    
    Change-Id: I232b3e2f27d53cef6abe812613edb6b86e8fe3e2

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -27,6 +27,7 @@ $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECI
 $(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_bool, Board_build_mtk_hals, $(filter true,$(BOARD_BUILD_MTK_HALS)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
