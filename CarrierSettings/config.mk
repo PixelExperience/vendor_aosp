72ee4f65a89deafab6e6ddab59b23d9a0c999c8a
Dyneteve <dyneteve@hentaios.com>
2020-11-03 10:07:41 +0100

    vendor: CarrierSettings: Update from RD1A.201105.003.C1
    Change-Id: Id76cc4ffc3ac3c7733886b27b18b3954e02c5579

--- CarrierSettings/config.mk
+++ CarrierSettings/config.mk
@@ -13,103 +13,104 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     default.pb
 
-# Australia
-PRODUCT_PACKAGES += \
-    optus_au.pb \
-    telstra_au.pb \
-    vodafone_au.pb
-
-# Canada
-PRODUCT_PACKAGES += \
-    bell_ca.pb \
-    fido_ca.pb \
-    fizz_ca.pb \
-    freedommobile_ca.pb \
-    koodo_ca.pb \
-    luckymobile_ca.pb \
-    pcmobilebell_ca.pb \
-    rogers_ca.pb \
-    solomobile_ca.pb \
-    telus_ca.pb \
-    videotron_ca.pb \
-    virgin_ca.pb
-
-# France
-PRODUCT_PACKAGES += \
-    bouygues_fr.pb \
-    orange_fr.pb \
-    sfr_fr.pb
-
-# Germany
-PRODUCT_PACKAGES += \
-    o2_de.pb \
-    telekom_de.pb \
-    vodafone_de.pb
-
-# India
+# Carriers Databases
 PRODUCT_PACKAGES += \
     airtel_in.pb \
-    idea_in.pb \
-    rjio_in.pb \
-    vodafone_in.pb
-
-# Italy
-PRODUCT_PACKAGES += \
-    vodafone_it.pb
-
-# Japan
-PRODUCT_PACKAGES += \
-    docomo_jp.pb \
-    kddi_jp.pb \
-    rakuten_jp.pb \
-    softbank_jp.pb
-
-# Netherlands
-PRODUCT_PACKAGES += \
-    vodafone_nl.pb
-
-# Singapore
-PRODUCT_PACKAGES += \
-    singtel_sg.pb \
-    starhub_sg.pb
-
-# Spain
-PRODUCT_PACKAGES += \
-    orange_es.pb \
-    vodafone_es.pb
-
-# Taiwan
-PRODUCT_PACKAGES += \
-    cht_tw.pb \
-    fet_tw.pb \
-    twm_tw.pb
-
-# United Kingdom
-PRODUCT_PACKAGES += \
-    ee_gb.pb \
-    h3_gb.pb \
-    idmobile_gb.pb \
-    o2postpaid_gb.pb \
-    o2prepaid_gb.pb \
-    vodafone_gb.pb
-
-# United States
-PRODUCT_PACKAGES += \
+    apt_tw.pb \
     att5g_us.pb \
     att_us.pb \
+    bell_ca.pb \
+    bluegrass_us.pb \
+    boost_us.pb \
+    bouygues_fr.pb \
+    btb_gb.pb \
+    btc_gb.pb \
     cellcom_us.pb \
+    cht_tw.pb \
+    congstar_de.pb \
     cricket5g_us.pb \
     cricket_us.pb \
     cspire_us.pb \
-    firstnetpacific_us.pb \
-    firstnet_us.pb \
+    docomo_jp.pb \
+    ee_gb.pb \
+    eplus_de.pb \
+    esn_gb.pb \
+    fet_tw.pb \
     fi_us.pb \
+    fido_ca.pb \
+    firstnet_us.pb \
+    firstnetpacific_us.pb \
+    fizz_ca.pb \
+    freedommobile_ca.pb \
+    h3_at.pb \
+    h3_gb.pb \
+    h3_se.pb \
+    idea_in.pb \
+    idmobile_gb.pb \
+    kddi_jp.pb \
+    kddimvno_jp.pb \
+    koodo_ca.pb \
+    luckymobile_ca.pb \
+    movistar_es.pb \
+    o2_de.pb \
+    o2postpaid_gb.pb \
+    o2prepaid_de.pb \
+    o2prepaid_gb.pb \
+    optus_au.pb \
+    orange_es.pb \
+    orange_fr.pb \
+    pcmobilebell_ca.pb \
+    rakuten_jp.pb \
+    rjio_in.pb \
+    rogers_ca.pb \
+    sfr_fr.pb \
+    shaw_ca.pb \
+    simple_us.pb \
+    singtel_sg.pb \
+    smarty_gb.pb \
+    softbank_jp.pb \
+    solomobile_ca.pb \
+    spectrum_us.pb \
     sprint_us.pb \
+    sprintprepaid_us.pb \
     sprintwholesale_us.pb \
+    starhub_sg.pb \
+    swisscom_ch.pb \
+    swisscom_li.pb \
+    tbaytel_ca.pb \
+    tdc_dk.pb \
+    tele2_se.pb \
+    telekom_de.pb \
+    telenor_dk.pb \
+    telenor_no.pb \
+    telenor_se.pb \
+    telia_no.pb \
+    telia_se.pb \
+    telstra_au.pb \
+    telus_ca.pb \
+    three_dk.pb \
+    tim_it.pb \
+    tmobile_nl.pb \
     tmobile_us.pb \
+    tracfonetmo_us.pb \
+    tracfoneverizon_us.pb \
+    tstar_tw.pb \
+    twm_tw.pb \
     uscc_us.pb \
     verizon_us.pb \
+    videotron_ca.pb \
+    virgin_ca.pb \
+    virgin_us.pb \
     visible_us.pb \
+    vodafone_au.pb \
+    vodafone_de.pb \
+    vodafone_es.pb \
+    vodafone_gb.pb \
+    vodafone_ie.pb \
+    vodafone_in.pb \
+    vodafone_it.pb \
+    vodafone_nl.pb \
+    vodafone_tr.pb \
     xfinity_us.pb
 
 # Other countries
