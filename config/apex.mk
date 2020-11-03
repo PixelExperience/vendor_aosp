4e1237786c5d8c37a9ef5297767ff4a36d7b7a9e
Dyneteve <dyneteve@hentaios.com>
2020-11-03 10:06:40 +0100

    vendor: Import apex from RD1A.201105.003.C1
    Change-Id: Ic74069324343cee7cece4a7a182cf76f35147d3c

---
+++ config/apex.mk
@@ -0,0 +1,26 @@
+# Enable Google Play system updates support
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/aosp/apex
+
+# ModuleMetadata
+PRODUCT_PACKAGES += \
+    ModuleMetadataGoogle
+
+# Google Apexes
+PRODUCT_PACKAGES += \
+    com.google.android.adbd \
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2 \
+    com.google.android.wifi
