dc89ca1711cad63870c234be63daf401a75e08ab
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:59:36 -0300

    vendor: Force latest redfin build fingerprint
    Change-Id: I57ba4cc2443c9fba0a7233e2d942de87c8ae6a9a

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,3 +1,6 @@
+# Safetynet
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys
+
 include vendor/aosp/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
