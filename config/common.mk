yashsonawane/platform_vendor_lucid

vendor: Add fonts for customization and set Google Sans as default font.
Signed-off-by: Arseniy Graur <argraur@aosip.dev>

configs/common.mk
@@ -36,6 +36,9 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+# Fonts
+include vendor/lucid/configs/fonts.mk
+
 # Common overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/lucid/overlay
 

PixelExperience/vendor_aosp

Change-Id: Iac080faf905985c38bdb15caaa90cdc6ec8d909c

config/common.mk
@@ -156,7 +156,7 @@ include vendor/aosp/config/ota.mk
 # GApps
 #include vendor/gapps/config.mk
 
-# Pixel Style
-#include vendor/pixelstyle/config.mk
+# Fonts
+include vendor/aosp/config/fonts.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk