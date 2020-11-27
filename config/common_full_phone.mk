9d940b1b080d810709845d8d59e8e9b4b2a67d7d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-26 22:39:52 -0300

    Don't build CellBroadcastReceiver
    That was removed on Android R
    
    Change-Id: I10d8fa8b1ae8a3730ee7c059be4ef80617ac9f8a

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,8 +8,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony packages
 PRODUCT_PACKAGES += \
-    Stk \
-    CellBroadcastReceiver
+    Stk
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
