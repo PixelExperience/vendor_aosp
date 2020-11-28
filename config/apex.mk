509d55815a7d73fb1107ef263a342506fb3ca50a
Chandra Chaganti <chandra.chaganti@hotmail.com>
2020-11-28 04:33:23 +0100

    vendor: apex: Don't ship cellbroadcast
    E AndroidRuntime: java.lang.IllegalStateException: Signature mismatch on system package com.google.android.cellbroadcastservice for shared user SharedUserSetting{6f0221a android.uid.networkstack/1073}
    
    Change-Id: I49a295047dcd6e558edf6220f69e6daf386df2f4

--- config/apex.mk
+++ config/apex.mk
@@ -15,7 +15,6 @@ PRODUCT_PACKAGES += \
 # Google Apexes
 PRODUCT_PACKAGES += \
     com.google.android.adbd \
-    com.google.android.cellbroadcast \
     com.google.android.conscrypt \
     com.google.android.extservices \
     com.google.android.ipsec \
