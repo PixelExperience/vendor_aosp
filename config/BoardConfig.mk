f5ac8dab672e086ea8b63d7f73e0d88df24b72da
Akash <akashniki@gmail.com>
2020-12-09 18:58:03 +0100

    aosp: Update redfin fingerprint to Dec one
    Change-Id: I5b6e2604007d468c520a5db92d4f60e805d383c9

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,5 +1,5 @@
 # Safetynet
-TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
 
 include vendor/aosp/config/BoardConfigKernel.mk
 
