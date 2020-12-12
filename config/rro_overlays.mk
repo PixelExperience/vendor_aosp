7f106e649382369e650b79ec131c8fd0fa14ae8d
Dyneteve <dyneteve@hentaios.com>
2020-12-13 02:27:13 +0900

    vendor: rro_overlays: Import CellBroadcastServiceOverlay
    * Allow safetyhub to send messages to cellbroadcast
    
    Change-Id: Ic655548ce3f2355f779ea32d9ce4fb2a7c77b3d2

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -4,6 +4,7 @@ PRODUCT_PACKAGES += \
     FilesOverlay \
     GooglePermissionControllerOverlay \
     PixelDocumentsUIGoogleOverlay \
+    CellBroadcastServiceOverlay \
     PixelTetheringOverlay \
     IconPackCircularPixelLauncherOverlay \
     IconPackCircularPixelThemePickerOverlay \
