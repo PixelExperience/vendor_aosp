28b9ed57e1d3c40c5f7b0d05d33d35da79d9becd
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-12-15 19:35:59 -0300

    Revert "vendor: apex: Don't ship cellbroadcast"
    This reverts commit 8e1649e3b89a1a74b993c5fd5b88960cd59a902c.
    
    Change-Id: I8c86617844ec7e0e7c840e2e6212acac0794b86a

--- config/apex.mk
+++ config/apex.mk
@@ -14,6 +14,7 @@ PRODUCT_PACKAGES += \
 # Google Apexes
 PRODUCT_PACKAGES += \
     com.google.android.adbd \
+    com.google.android.cellbroadcast \
     com.google.android.conscrypt \
     com.google.android.extservices \
     com.google.android.ipsec \
