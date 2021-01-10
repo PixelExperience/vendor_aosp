fcbf70c19e68e05e9e3eabbb8092ac454f303c2b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-01-10 15:02:15 -0300

    Revert "vendor: Guard qcom dataservices for sodp"
    This reverts commit 1957c7e05f3ed706a6702e9044f6357c680b320e.
    
    Change-Id: I09711c780e01d9371556c53156e8f0f945bf644d

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -112,8 +112,3 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
     vendor/qcom/opensource/commonsys/system/bt/conf
 endif #TARGET_USE_QTI_BT_STACK
-
-# Add dataservices to PRODUCT_SOONG_NAMESPACES if needed
-ifneq ($(PRODUCT_PLATFORM_SOD),true)
-    PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/dataservices
-endif
