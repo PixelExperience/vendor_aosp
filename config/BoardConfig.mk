50530c5d9020656fbf9810c9c96d3170f90814cd
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-03-11 15:23:40 +0100

    Define our avb key path if target supports custom avb key
    Change-Id: I29e21e0f60e37ab98dc32b9afcd92616edb0adb2

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -4,6 +4,13 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/aosp/config/BoardConfigQcom.mk
 endif
 
+# Custom AVB Key
+ifneq ($(filter OFFICIAL CI,$(CUSTOM_BUILD_TYPE)),)
+ifeq ($(TARGET_USES_CUSTOM_AVB_KEY),true)
+include vendor/aosp/config/BoardConfigAvb.mk
+endif
+endif
+
 include vendor/aosp/config/BoardConfigSoong.mk
 
 # Namespace for fwk-detect
---
+++ config/BoardConfigAvb.mk
@@ -0,0 +1,5 @@
+BOARD_AVB_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_KEY_PATH := external/avb-keys/avb.pem
+ifneq ($(BOARD_AVB_VBMETA_SYSTEM_KEY_PATH), )
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb-keys/avb.pem
+endif
