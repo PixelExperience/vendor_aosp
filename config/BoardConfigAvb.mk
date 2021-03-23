539981ac10ac59947e95609011e78cbd281caad5
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-03-23 14:45:58 +0100

    Also set avb key path for recovery
    * For any A only devices that may want to support this
    
    Change-Id: I5838056ad7416707b061bb5aa90b2e306537708f
    (cherry picked from commit aea5dc6a14c65448bf61478d48c0b7fb5d89bcbb)

--- config/BoardConfigAvb.mk
+++ config/BoardConfigAvb.mk
@@ -3,3 +3,6 @@ BOARD_AVB_KEY_PATH := external/avb-keys/avb.pem
 ifneq ($(BOARD_AVB_VBMETA_SYSTEM_KEY_PATH), )
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb-keys/avb.pem
 endif
+ifneq ($(BOARD_AVB_RECOVERY_KEY_PATH), )
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb-keys/avb.pem
+endif
