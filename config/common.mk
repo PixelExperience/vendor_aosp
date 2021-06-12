68e8ccfbd122bbcde73fc04bab8f228ce86f36f7
Jarl-Penguin <jarlpenguin@outlook.com>
2021-06-12 16:06:47 +0000

    vendor: Include vendor/extra if it exists again
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Icba6e2a6853e1937fa6f8ad962e87415111b8f4c

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,6 @@
+# Allow vendor/extra to override any property by setting it first
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
 PRODUCT_BRAND ?= PixelExperience
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
