91d25efc75797b7c93bc68a85614d3e37beda7ff
jhenrique09 <jhsv09@gmail.com>
2021-10-09 20:14:45 -0300

    [REVERTME] - Switch to staging
    Change-Id: I9c86a346998a92a88086a2ad49dd62bf61ac6b1a

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -155,7 +155,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to PixelExperience Gerrit
-    default_gerrit = 'https://gerrit.pixelexperience.org'
+    default_gerrit = 'https://gerrit-staging.pixelexperience.org'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
