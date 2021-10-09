9eb62d969eaba9b89e6e2c4473285db899adf112
jhenrique09 <jhsv09@gmail.com>
2021-10-09 20:13:51 -0300

    Initial twelve bringup
    Change-Id: I874845c170b5cf4d30de15b2f5955240f4399424

--- tools/get_official_devices.py
+++ tools/get_official_devices.py
@@ -19,7 +19,7 @@ try:
     data = json.loads(response.read())
     for res in data:
         for version in res['supported_versions']:
-            if version['version_code'] == 'eleven':
+            if version['version_code'] == 'twelve':
                 print (res['codename'])
                 break
 except:
