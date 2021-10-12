f1eaae5f84cc5d1d3ce91a58a1b80a72b39e5974
jhenrique09 <jhsv09@gmail.com>
2021-10-12 20:12:31 -0300

    vendor: Include pixel-framework if exists
    Change-Id: I9dfb78f752132d535dcc0c41848f3e1bd77ac88a

--- config/common.mk
+++ config/common.mk
@@ -214,4 +214,7 @@ $(call inherit-product, vendor/aosp/config/ota.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Pixel Framework
+$(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
