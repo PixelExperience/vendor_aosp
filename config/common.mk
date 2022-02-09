a4cf29fa7813825ea2fa18347fdf31871ee78366
jhenrique09 <jhsv09@gmail.com>
2022-02-09 07:53:56 -0300

    vendor: Define adb prop on product partition too
    Change-Id: I5f6f57e168c8d88d02c38becebe9e0478d5b0933

--- config/common.mk
+++ config/common.mk
@@ -26,9 +26,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+PRODUCT_PRODUCT_PROPERTIES += ro.adb.secure=0
 else
 # Enable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+PRODUCT_PRODUCT_PROPERTIES += ro.adb.secure=1
 
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
