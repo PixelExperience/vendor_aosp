f238c3289d83bae979774a49cc25327702ac288d
Bruno Martins <bgcngm@gmail.com>
2022-03-11 12:10:45 +0000

    config: Create makefiles for tablets with telephony support
    So far, tablets with telephony have been inheriting from
    common_full_phone.mk, but that's not really proper because
    such makefile now enables one-handed mode support
    (setting prop ro.support_one_handed_mode to true).
    
    Fix this by creating new makefiles that can be used also
    to include more tablet-specific configs moving forward.
    
    Change-Id: I90c22badb17911ef5e873299d986204718300e6c

---
+++ config/common_full_tablet.mk
@@ -0,0 +1,10 @@
+# World APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk
+
+# Inherit full common PixelExperience stuff
+$(call inherit-product, vendor/aosp/config/common_full.mk)
