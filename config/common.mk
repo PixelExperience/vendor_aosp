e7591c8efbd534ccc9e9cf21c893e8e0d63a4a03
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-04-01 23:53:33 +0000

    vendor: common: Inherit ThemeIcons
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ibb697ff07a05598efe2231f2d0330fa8f09f42a6

--- config/common.mk
+++ config/common.mk
@@ -212,4 +212,7 @@ $(call inherit-product, vendor/aosp/config/ota.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# ThemeIcons Overlay
+$(call inherit-product, packages/overlays/ThemeIcons/config.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
