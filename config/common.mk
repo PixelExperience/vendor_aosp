8ae3191121d3d164552b481b09560ec4b215feec
jhenrique09 <jhsv09@gmail.com>
2022-04-06 22:36:21 +0000

    vendor: Build DummyCutoutOverlay
    Change-Id: I570f39084e3c7b0c60e9c82539ad8ea4d2d283b2

--- config/common.mk
+++ config/common.mk
@@ -102,7 +102,8 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 # Cutout control overlay
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+    NoCutoutOverlay \
+    DummyCutoutOverlay
 
 # TouchGestures
 PRODUCT_PACKAGES += \
