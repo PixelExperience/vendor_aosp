6de298d7dd0620949e8977d303f79a63b7159c11
jhenrique09 <jhsv09@gmail.com>
2022-04-08 01:51:30 +0000

    vendor: Build RepainterServicePriv
    Change-Id: Iaa340e3947c9580e623077b011766a8a81235d28

--- config/common.mk
+++ config/common.mk
@@ -205,6 +205,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Repainter integration
+PRODUCT_PACKAGES += \
+    RepainterServicePriv
+
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)
 
