912f806d873ca84a95710695a22ce26536f55162
jhenrique09 <jhsv09@gmail.com>
2022-05-23 21:18:20 +0000

    vendor: Allow using legacy fd
    Change-Id: I88e355e60de63a7f81adf8e25fabbd305fb0af7c
    (cherry picked from commit 56ca19f62683ae28f0803471d3be0e156b1790ff)

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -40,7 +40,8 @@ SOONG_CONFIG_customGlobalVars += \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
     target_uses_prebuilt_dynamic_partitions \
-    uses_camera_parameter_lib
+    uses_camera_parameter_lib \
+    uses_legacy_fd_fbdev
 
 SOONG_CONFIG_NAMESPACES += customNvidiaVars
 SOONG_CONFIG_customNvidiaVars += \
@@ -81,6 +82,7 @@ SOONG_CONFIG_customQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_customQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_customQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+SOONG_CONFIG_customGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
