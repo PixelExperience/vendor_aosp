e7cca91f32684dfebfa436a09f2ff04e5fa5dc8d
jhenrique09 <jhsv09@gmail.com>
2022-08-02 00:11:05 +0000

    vendor: Nuke 720p bootanimation and force 1080p as default
    Requires https://review.lineageos.org/c/LineageOS/android_frameworks_base/+/330313
    
    and https://gerrit.pixelexperience.org/c/frameworks_base/+/19240
    
    Change-Id: I77deebdfbf6de89ce3e881c5f45285fb9497b3bb

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -4,8 +4,6 @@ ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
      PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
     ifeq ($(TARGET_BOOT_ANIMATION_RES),)
         $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
