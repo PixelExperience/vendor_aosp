8151560c82f9e09afb07473279d5a58658a91e43
jhenrique09 <jhsv09@gmail.com>
2022-08-10 15:54:35 +0000

    vendor: Add CUSTOM_SECURITY_PATCH prop
    Change-Id: I0e690874ee9d3a783719494ea0e5bc3ece6cbf13
    (cherry picked from commit 72b376f161c5686140fb1d29f2d0176451b43580)

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -18,10 +18,14 @@ ADDITIONAL_SYSTEM_PROPERTIES  += \
     persist.sys.recovery_update=true
 endif
 
+# Custom security patch
+CUSTOM_SECURITY_PATCH := 2022-08-05
+
 # Versioning props
 ADDITIONAL_SYSTEM_PROPERTIES  += \
     org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
     org.pixelexperience.version.display=$(CUSTOM_VERSION) \
     org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
     org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE)
+    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE) \
+    org.pixelexperience.build_security_patch=$(CUSTOM_SECURITY_PATCH)
