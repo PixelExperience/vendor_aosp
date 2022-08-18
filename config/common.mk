c86004996890588c9e12594bfa804ab8a75bc97a
jhenrique09 <jhsv09@gmail.com>
2022-08-18 17:54:19 -0300

    [REVERTME] vendor: init: Hide permissive selinux for SafetyNet
    Change-Id: Iac037a600b63ca2ed621344faf90dbdd362bc69b

--- config/common.mk
+++ config/common.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
 
 # Copy all custom init rc files
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init/init.pixelexperience-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.pixelexperience-updater.rc
+    vendor/aosp/prebuilt/common/etc/init/init.pixelexperience-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.pixelexperience-updater.rc \
+    vendor/aosp/prebuilt/common/etc/init/init.snet.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.snet.rc
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
