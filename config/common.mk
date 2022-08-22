c998dac7b878c78a1a832cfb25fc9362c8e6e939
jhenrique09 <jhsv09@gmail.com>
2022-08-21 23:40:51 -0300

    vendor: Ensure that priv-app permission is not enforced
    Change-Id: I7ac03426449be03acce42b6bbf95564171f2ca87

--- config/common.mk
+++ config/common.mk
@@ -57,8 +57,8 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
 
 # Enforce privapp-permissions whitelist
-#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-#    ro.control_privapp_permissions=enforce
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.control_privapp_permissions=log
 
 # Power whitelist
 PRODUCT_COPY_FILES += \
