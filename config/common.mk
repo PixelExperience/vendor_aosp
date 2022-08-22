2386980fc1d452aed5d21dd31d9972532434dfe2
jhenrique09 <jhsv09@gmail.com>
2022-08-22 20:23:08 -0300

    vendor: Enable pixel stuffs
    Change-Id: I3fb886da6adcc56d022a9d203d8cd4eec595bacb

--- config/common.mk
+++ config/common.mk
@@ -202,15 +202,15 @@ $(call inherit-product, vendor/aosp/config/audio.mk)
 $(call inherit-product, vendor/aosp/config/bootanimation.mk)
 
 # Fonts
-#$(call inherit-product, vendor/aosp/config/fonts.mk)
+$(call inherit-product, vendor/aosp/config/fonts.mk)
 
 # GApps
-#$(call inherit-product, vendor/gapps/config.mk)
+$(call inherit-product, vendor/gapps/config.mk)
 
 # OTA
 #$(call inherit-product, vendor/aosp/config/ota.mk)
 
 # RRO Overlays
-#$(call inherit-product, vendor/aosp/config/rro_overlays.mk)
+$(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
