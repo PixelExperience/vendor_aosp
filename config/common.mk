LineageOS/android_vendor_lineage

config: Exclude AOSP rild package from enforcement
Devices using vendor RIL services (ENABLE_VENDOR_RIL_SERVICE)
encounter a dead end when enforcing product package existance.

Change-Id: I101ebf442e35c631a6ff9f352566de63dd5cfba7

config/common.mk
@@ -101,7 +101,7 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild)
 
 # Bootanimation
 TARGET_SCREEN_WIDTH ?= 1080

PixelExperience/vendor_aosp

config/common.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/aosp/config/branding.mk)
 
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(CUSTOM_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(CUSTOM_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild)
 
 PRODUCT_BRAND ?= PixelExperience
 