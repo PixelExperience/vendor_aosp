5802859e993ad44236b27a80df539310f05b5cb8
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-12-13 17:18:52 +0530

    vendor: GMS: Refactor setting of PRODUCT_GMS_CLIENTID_BASE
    Change-Id: If9fb45287041a61dc835c2da1d81ebbeec4bdcc0
    (cherry picked from commit 00432be46770dabae9d23d1d8e17f4b316571f43)

--- config/common.mk
+++ config/common.mk
@@ -8,9 +8,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google
+
 else
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+
 endif
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -164,7 +172,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Google Play services configuration
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.clientidbase=android-google \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
 
