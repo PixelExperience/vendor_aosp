775af60664fe64b16066f0feac12d5a48a66d573
Alcatraz323 <alcatraz32323@gmail.com>
2023-02-08 16:56:57 +0800

    vendor: fix NexusLauncher & introduce common_full_tablet flavor
    Change-Id: If648169037b7e2fa49c5dd88939a1ee8a612cebd

---
+++ config/common_full_tablet.mk
@@ -0,0 +1,20 @@
+# Tablet extension
+PRODUCT_PACKAGES += \
+    NexusLauncherTabletOverlay \
+    androidx.window.extensions
+
+# World APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk
+
+# Tethering - allow without requiring a provisioning app
+# (for devices that check this)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    net.tethering.noprovisioning=true
+
+# Inherit full common PixelExperience stuff
+$(call inherit-product, vendor/aosp/config/common_full.mk)
--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,2 +1,7 @@
+# Tablet extension
+PRODUCT_PACKAGES += \
+    NexusLauncherTabletOverlay \
+    androidx.window.extensions
+
 # Inherit full common PixelExperience stuff
 $(call inherit-product, vendor/aosp/config/common_full.mk)
