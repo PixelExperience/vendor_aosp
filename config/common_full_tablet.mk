83799965f57871114a5d0b08b0ebf7dbb4a11079
Alcatraz323 <alcatraz32323@gmail.com>
2023-02-08 16:56:57 +0800

    vendor: fix NexusLauncher for tablet & introduce common_full_tablet flavor
    Change-Id: If648169037b7e2fa49c5dd88939a1ee8a612cebd

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -7,4 +7,4 @@ PRODUCT_PACKAGES += \
     androidx.window.extensions
 
 # Inherit full common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
