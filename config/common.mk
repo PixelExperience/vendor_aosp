2b7c7f7e800dff45f57d47fec333f0c2d1b545a4
Alcatraz323 <alcatraz32323@gmail.com>
2023-02-15 19:52:10 +0800

    vendor: fix NexusLauncher for tablet mode & follow system taskbar size
    Change-Id: If648169037b7e2fa49c5dd88939a1ee8a612cebd

--- config/common.mk
+++ config/common.mk
@@ -220,6 +220,10 @@ endif
 PRODUCT_PACKAGES += \
     RepainterServicePriv
 
+# NexusLauncher resources
+PRODUCT_PACKAGES += \
+    NexusLauncherResOverlay
+
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)
 
