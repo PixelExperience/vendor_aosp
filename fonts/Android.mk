ad8eb1df21abbfa730288fff45cdad588338c16b
aswin7469 <aswinas@pixysos.com>
2023-06-15 18:38:50 +0530

    fonts: Update from TQ3A.230605.012
    Change-Id: I4386110d5a29bf8d3625a60f004bc99f2f755047
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- fonts/Android.mk
+++ fonts/Android.mk
@@ -36,7 +36,8 @@ endef
 font_src_files := \
     GoogleSans-Italic.ttf \
     GoogleSans-Regular.ttf \
-    GoogleSansClock-Regular.ttf
+    GoogleSansClock-Regular.ttf \
+    GoogleSansFlex-Regular.ttf
 
 $(foreach f, $(font_src_files), $(call build-one-font-module, $(f)))
 
