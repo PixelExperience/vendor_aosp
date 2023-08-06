1f35ebf426b7dde98ad0c4dc44ceec396cd34d9e
Henrique Silva <jhsv09@gmail.com>
2023-08-06 19:11:51 -0300

    vendor: Fix env variable conflict

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 lunch_others_targets=()
-for device in $(python vendor/aosp/tools/get_official_devices.py)
+for d in $(python vendor/aosp/tools/get_official_devices.py)
 do
-    for var in user userdebug eng; do
-        lunch_others_targets+=("aosp_$device-$var")
+    for v in user userdebug eng; do
+        lunch_others_targets+=("aosp_$d-$v")
     done
 done
