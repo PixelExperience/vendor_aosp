77b4340fa726975c9c8589270e7dcf989fb927a5
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-08-31 16:59:53 +0000

    config: qcom: Allow Skip common display vendor freezer through a condition
    * Vendor freezer is enabled for other SoC besides SM8450 on SM8550, just have S or T vendor blobs,
      Example: sdm845, sdm660 and bengal-5.15.
    
    * Enable by default to SM8450 and SM8550
    
    Change-Id: I4bf0115df21a1bd15aa789297f82ddf4994a7923
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -214,7 +214,11 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys/display \
     vendor/qcom/opensource/commonsys-intf/display
 
-ifeq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
+TARGET_USE_DISPLAY_VENDOR_FREEZER := true
+endif
+
+ifneq ($(TARGET_USE_DISPLAY_VENDOR_FREEZER),true)
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/display
 endif
