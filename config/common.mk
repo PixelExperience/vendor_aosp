2869c51cfdea99ee446c5b6d4fe99ecd206f721c
Michael Bestas <mkbestas@lineageos.org>
2023-10-09 16:10:05 +0300

    RIP Android Beam
    Change-Id: Ia977097d40b7f0538f2b15d231e69f2fa22adc4c

--- config/common.mk
+++ config/common.mk
@@ -52,10 +52,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init/init.pixelexperience-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.pixelexperience-updater.rc
 
-# Enable Android Beam on all targets
-PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
-
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
