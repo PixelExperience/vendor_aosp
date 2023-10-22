5041f1d8f0f2fb3082902d6bee03970b6b2533bb
inferno0230 <adityachoudhury0230@gmail.com>
2023-10-22 07:28:32 +0530

    tools: Update custom_apns script for Python 3 compatibility
    explicitly specify the encoding='utf-8' parameter when opening files
    
    replace reload() function with importlib as its no longer a builtin funtion in python 3 [1].
    
    [1] https://docs.python.org/3/library/functions.html
    
    Change-Id: I2de2b5b5a74e8295d34e40237b3f8cc7abeacad8

--- tools/custom_apns.py
+++ tools/custom_apns.py
@@ -17,10 +17,10 @@
 
 import re
 import sys
+import importlib
 
 def main(argv):
-    reload(sys)
-    sys.setdefaultencoding('utf8')
+    importlib.reload(sys)
     original_file = 'vendor/aosp/prebuilt/common/etc/apns-conf.xml'
 
     if len(argv) == 3:
@@ -30,17 +30,17 @@ def main(argv):
         raise ValueError("Wrong number of arguments %s" % len(argv))
 
     custom_apn_names = set()
-    with open(custom_override_file, 'r') as f:
+    with open(custom_override_file, 'r', encoding='utf-8') as f:
         for line in f:
             custom_apn_names.add(re.search(r'carrier="[^"]+"', line).group(0))
 
-    with open(original_file, 'r') as input_file:
-        with open(output_file_path, 'w') as output_file:
+    with open(original_file, 'r', encoding='utf-8') as input_file:
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
             for line in input_file:
                 found_custom_apns = set()
                 for apn in custom_apn_names:
                     if apn in line:
-                        with open(custom_override_file, 'r') as custom_file:
+                        with open(custom_override_file, 'r', encoding='utf-8') as custom_file:
                             for override_line in custom_file:
                                 if apn in override_line:
                                     output_file.write(override_line)
@@ -51,7 +51,7 @@ def main(argv):
                     if "</apns>" in line:
                         if custom_apn_names:
                             for apn in custom_apn_names:
-                                with open(custom_override_file, 'r') as custom_file:
+                                with open(custom_override_file, 'r', encoding='utf-8') as custom_file:
                                     for override_line in custom_file:
                                         if apn in override_line:
                                             output_file.write(override_line)
